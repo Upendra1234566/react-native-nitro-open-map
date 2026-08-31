6efd092e143705b0e538c0568ef338fe8f9f18fe
Upendra Singh <us740413@gmail.com>
2026-08-31 12:05:14 +0530

    chore: update projec

--- src/NitroOpenMapView.native.tsx
+++ src/NitroOpenMapView.native.tsx
@@ -1,30 +1,3 @@
-// import { type ViewProps } from 'react-native';
-// import { getHostComponent } from 'react-native-nitro-modules';
-// import type {
-//   NitroOpenMapMethods,
-//   NitroOpenMapProps,
-//   Marker,
-// } from './NitroOpenMap.nitro';
-
-// const NitroOpenMapConfig = require('../nitrogen/generated/shared/json/NitroOpenMapConfig.json');
-
-// const NativeMapComponent = getHostComponent<
-//   NitroOpenMapProps,
-//   NitroOpenMapMethods
-// >('NitroOpenMap', () => NitroOpenMapConfig);
-
-// export type MapProps = ViewProps & {
-//   color: string;
-//   latitude?: number;
-//   longitude?: number;
-//   zoom?: number;
-//   mapStyle?: string;
-//   markers?: Marker[];
-// };
-
-// export function NitroOpenMapView({ color = '#FFFFFF', ...props }: MapProps) {
-//   return <NativeMapComponent {...props} color={color} />;
-// }
 import { getHostComponent } from 'react-native-nitro-modules';
 import type {
   NitroOpenMapMethods,
--- src/NitroOpenMapView.tsx
+++ src/NitroOpenMapView.tsx
@@ -1,22 +1,3 @@
-// // import { View, type ColorValue, type ViewProps } from 'react-native';
-
-// // type Props = ViewProps & {
-// //   color?: ColorValue;
-// //   latitude?: number;
-// //   longitude?: number;
-// // };
-
-// // export function NitroOpenMapView({ color, style, ...rest }: Props) {
-// //   return <View {...rest} style={[style, { backgroundColor: color }]} />;
-// // }
-// import { View, type ViewProps } from 'react-native';
-// import type { NitroOpenMapProps } from './NitroOpenMap.nitro';
-
-// export type MapProps = ViewProps & NitroOpenMapProps;
-
-// export function NitroOpenMapView(props: MapProps) {
-//   return <View {...(props as any)} />;
-// }
 import { getHostComponent } from 'react-native-nitro-modules';
 import type {
   NitroOpenMapMethods,
